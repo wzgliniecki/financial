06d12fe8132cddfd5ecffa50989607f96056cecc
Wojciech Zgliniecki <wojciech.zgliniecki@gmail.com>
2025-04-13 18:42:35 +0200

    Basic TRPC code on backend.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia, t as T } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { initTRPC } from '@trpc/server'
+import { compile as c, trpc } from '@elysiajs/trpc'
+
+const t = initTRPC.create()
+const p = t.procedure
+
+const router = t.router({
+	greet: p
+		// 💡 Using Zod
+		//.input(z.string())
+		// 💡 Using Elysia's T
+		.input(c(T.String()))
+		.query(({ input }) => input)
+})
+
+export type Router = typeof router
+
+const app = new Elysia().get("/", () => "Hello Elysia").use(trpc(router)).listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
