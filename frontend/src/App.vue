a03a80a83e25f99ffe78a25543cf3b33da3549fd
Wojciech Zgliniecki <wojciech.zgliniecki@gmail.com>
2025-04-13 19:32:45 +0200

    Basic charts added.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import LoanTable from './components/LoanTable.vue';
+import LinearChart from './components/LinearChart.vue';
+
+const data = ref({
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [
+    {
+      label: 'Loan Amount',
+      data: [40, 50, 30, 60, 90],
+      fill: false,
+      borderColor: '#42b983',
+      tension: 0.1
+    }
+  ]
+});
+
+const options = ref({
+  responsive: true,
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+});
 </script>
 
 <template>
@@ -7,9 +31,10 @@ import LoanTable from './components/LoanTable.vue';
   </header>
   <main>
     <LoanTable />
+    
+    <LinearChart :chartData="data" :chartOptions="options"/>
   </main>
 </template>
 
 <style scoped>
-
 </style>
---
+++ frontend/src/components/LinearChart.vue
@@ -0,0 +1,60 @@
+<template>
+  <Line
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { ref } from 'vue'
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale
+)
+
+export default {
+  name: 'LinearChart',
+  components: { Line },
+  setup() {
+    const chartData = ref({
+      labels: ['January', 'February', 'March'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: [40, 20, 12],
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.4
+        }
+      ]
+    })
+
+    const chartOptions = ref({
+      responsive: true
+    })
+
+    return {
+      chartData,
+      chartOptions
+    }
+  }
+}
+</script>
+
